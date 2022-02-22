06c3df24a12f42029ba25057f404ce0b2ebbccf1
Nishant Mittal <mittalnishant14@outlook.com>
2022-02-22 22:50:47 +0530

    feat(api-service): implement auth endpoints

--- Makefile
+++ Makefile
@@ -7,6 +7,6 @@ endif
 
 
 compile-protos:
-	python -m grpc_tools.protoc -I=. --python_out=./api-service --grpc_python_out=./api-service ./protobuf/auth.proto && \
+	python -m grpc_tools.protoc -I=. --python_out=./api-service --grpc_python_out=./api-service ./protobuf/auth.proto ./protobuf/todo.proto && \
 	$(protoc_binary) -I=. --go_out=./auth-service --go_opt=paths=source_relative --go-grpc_out=./auth-service --go-grpc_opt=paths=source_relative ./protobuf/auth.proto && \
 	mkdir -p todo-service/protobuf && cp protobuf/todo.proto todo-service/protobuf/todo.proto
--- api-service/app.py
+++ api-service/app.py
@@ -1,25 +1,90 @@
 import grpc
-from flask import Flask
-from protobuf import auth_pb2, auth_pb2_grpc
+import os
+from typing import Optional
+from fastapi import FastAPI, HTTPException, Security
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from pydantic import BaseModel
+from dotenv import load_dotenv
+from protobuf import auth_pb2, auth_pb2_grpc, todo_pb2_grpc, todo_pb2
 
-app = Flask(__name__)
-auth_service_channel = grpc.insecure_channel('localhost:3005')
+load_dotenv()
+
+AUTH_SERVICE_URL = os.environ['AUTH_SERVICE_URL']
+TODO_SERVICE_URL = os.environ['TODO_SERVICE_URL']
+
+app = FastAPI(
+    title="API Service (Todo App)",
+    version="0.1.0",
+    docs_url="/"
+)
+security = HTTPBearer()
+
+auth_service_channel = grpc.insecure_channel(AUTH_SERVICE_URL)
 auth_service_stub = auth_pb2_grpc.AuthServiceStub(auth_service_channel)
+todo_service_channel = grpc.insecure_channel(TODO_SERVICE_URL)
+todo_service_stub = todo_pb2_grpc.TodoServiceStub(todo_service_channel)
 
-@app.get("/")
-def home():
-    token = auth_service_stub.RegisterUser(auth_pb2.UserRegistrationForm(username="some", name="name", password="asdas"))
-    print(token.token)
-    return "<p>Todo App API Service</p>"
+class UserProfile(BaseModel):
+    username: str
+    name: str
 
-@app.post("/auth/register")
-def register_user():
-    return "<p>Hello, World!</p>"
+class RegisterUser(BaseModel):
+    username: str
+    name: str
+    password: str
 
-@app.post("/auth/login")
-def login_user():
-    return "<p>Hello, World!</p>"
+class LoginUser(BaseModel):
+    username: str
+    password: str
 
-@app.get("/auth/profile")
-def get_profile():
-    return "<p>Hello, World!</p>"
+class AuthToken(BaseModel):
+    token: str
+
+class Todo(BaseModel):
+    id: str
+    title: str
+
+def get_user(token: str):
+    try:
+        return auth_service_stub.AuthenticateUser(auth_pb2.Token(token=token))
+    except grpc.RpcError:
+        raise HTTPException(
+            status_code=401,
+            detail="invalid token"
+        )
+
+@app.post("/auth/login", response_model=AuthToken, tags=["auth"])
+def login(user: LoginUser):
+    try:
+        token = auth_service_stub.LoginUser(auth_pb2.UserCredentials(username=user.username, password=user.password))
+        return {"token": token.token}
+    except grpc.RpcError as e:
+        raise HTTPException(status_code=400, detail=e.details())
+
+@app.post("/auth/register", response_model=AuthToken, tags=["auth"])
+def register(user: RegisterUser):
+    try:
+        token = auth_service_stub.RegisterUser(auth_pb2.UserRegistrationForm(username=user.username, password=user.password, name=user.name))
+        return {"token": token.token}
+    except grpc.RpcError as e:
+        raise HTTPException(status_code=400, detail=e.details())
+
+@app.get("/auth/profile", response_model=UserProfile, tags=["auth"])
+def profile(token: HTTPAuthorizationCredentials = Security(security)):
+    user = get_user(token.credentials)
+    return {
+        "name": user.name,
+        "username": user.username,
+    }
+
+@app.get("/todos/{id}", response_model=Todo)
+def get_todo(id: str, token: HTTPAuthorizationCredentials = Security(security)):
+    user = get_user(token.credentials)
+    try:
+        todo = todo_service_stub.GetTodo(todo_pb2.TodoIdentifier(id=id, username=user.username))
+        return {
+            "id": todo.id,
+            "title": todo.title
+        }
+    except:
+        raise HTTPException(status_code=404, detail="not found")
